74f0ef02f0bf6bd1bf5d567eb30e688ea69ae2f6
unknown <KALIBU@JLPC1.Kalibu.org>
2022-03-23 14:29:17 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// Toggle button functionality
+const toggleButton = document.getElementsByClassName('toggle-button')[0] 
+const links = document.getElementsByClassName('links')[0]
+
+toggleButton.addEventListener('click', () => {
+    links.classList.toggle('active')
+})
